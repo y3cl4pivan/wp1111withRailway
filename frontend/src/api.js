bf2d0dee1367c4e2b1ba31f2cdaaf9d4f0522f38
Ivan0929 <96767129+y3cl4pivan@users.noreply.github.com>
2022-12-26 18:14:48 +0800

    Add files via upload

---
+++ frontend/src/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_ROOT = process.env.NODE_ENV === "production" ? "" : "http://localhost:4000/";
+
+const instance = axios.create({
+  baseURL: API_ROOT
+});
+
+export default instance;
+
